c0285bf045357eded2d7db4101b45f6a91543c2d
Phillip Faber <pfaber87@gmail.com>
2025-07-18 10:46:54 +0200

    Edit button feature added to main.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,64 @@
 import './style.scss';
 
-let userTaskInput = document.querySelector('.userTaskInput');
+class Task {
+  constructor(value) {
+    this.value = value;
+    this.listItem = document.createElement('li');
+    this.textSpan = document.createElement('span');
+    this.delBtn = document.createElement('button');
+    this.editBtn = document.createElement('button');
+
+    this.textSpan.textContent = this.value;
+    this.editBtn.textContent = "Edit";
+    this.delBtn.textContent = "X";
+
+    this.listItem.append(this.textSpan, this.editBtn, this.delBtn);
+
+    this.setupListeners();
+  }
+
+  setupListeners() {
+    this.delBtn.addEventListener('click', () => {
+      this.listItem.remove();
+    });
+
+    this.editBtn.addEventListener('click', () => {
+      if (this.editBtn.textContent === 'Edit') {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = this.value;
+
+        this.listItem.replaceChild(input, this.textSpan);
+        this.editBtn.textContent = "Save";
+
+        input.focus();
+
+      } else {
+        const input = this.listItem.querySelector('input');
+        this.value = input.value.trim();
+        this.textSpan.textContent = this.value;
+        this.listItem.replaceChild(this.textSpan, input);
+        this.editBtn.textContent = 'Edit';
+      }
+    });
+  }
+  render(container) {
+    container.appendChild(this.listItem);
+  }
+}
+
+const userTaskInput = document.querySelector('.userTaskInput');
+const taskList = document.querySelector('.task-list');
 const addBtn = document.querySelector('.add-btn');
-let taskList = document.querySelector('.task-list');
 
 
-addBtn.addEventListener('click', createToDo);
-
-function createToDo() {
+addBtn.addEventListener('click', () => {
   const value = userTaskInput.value.trim();
   if (value === "") return;
 
-  const listItem = document.createElement('li');
-  const textSpan = document.createElement('span');
-  const delBtn = document.createElement('button');
+  const task = new Task(value);
+  task.render(taskList);
 
-  textSpan.textContent = value;
-  delBtn.textContent = "X";
-
-  delBtn.addEventListener('click', () => {
-    listItem.remove();
-  });
-  
-  listItem.appendChild(textSpan);
-  listItem.appendChild(delBtn);
-  taskList.appendChild(listItem);
-  
   userTaskInput.value = "";
-}
+});
+    
